376f0328d39028d27def8e77af70a589eefe4ffe
Jonathan Gamble <jig1314@gmail.com>
2022-01-26 00:07:45 -0500

    Fixing bug in Counter script

--- NaturalSelectionSimulation/Assets/NaturalSelectionSimulation/_Dev/DevJonathan/Counter.cs
+++ NaturalSelectionSimulation/Assets/NaturalSelectionSimulation/_Dev/DevJonathan/Counter.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Counter : MonoBehaviour
 {
-    public int Counter { get; set; }
+    public int counter;
 
     // Start is called before the first frame update
     void Start()
     {
-        Counter = 0;
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Counter++;
+        counter++;
     }
 }
