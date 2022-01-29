f7ebf3aa5ecffbac96d24100f422f17ed79a3e71
Brett Cohen <fiziksbrett@gmail.com>
2022-01-28 23:50:35 -0800

    Created my dev folder and file

---
+++ NaturalSelectionSimulation/Assets/NaturalSelectionSimulation/_Dev/DevBrett/DEV_Brett.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEV_Brett : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("WANNA PLAY A GAME!!!!");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
