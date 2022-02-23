0e3b103d83bb8e8f343e70088ca16c0eb831801d
Jonathan Gamble <jig1314@gmail.com>
2022-02-23 02:42:33 -0500

    Added Options Menu

--- Assets/NaturalSelectionSimulation/_Dev/DevJonathan/MenuController.cs
+++ Assets/NaturalSelectionSimulation/_Dev/DevJonathan/MenuController.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 namespace NaturalSelectionSimulation
 {
     public class MenuController : MonoBehaviour
     {
+        #region Display Settings
+
+        [Header("Screen Mode Dropdown")]
+        private TMP_Dropdown screenModeDropdown;
+
+        #endregion
+
+        private void Start()
+        {
+            //screenModeDropdown.ClearOptions();
+
+            //var screenModes = new List<string>()
+            //{
+            //    "Fullscreen",
+            //    "Windowed",
+            //    "Borderless"
+            //};
+
+            //screenModeDropdown.AddOptions(screenModes);
+            //screenModeDropdown.value = 0;
+            //screenModeDropdown.RefreshShownValue();
+        }
+
         public void ExitGame()
         {
             Application.Quit();
