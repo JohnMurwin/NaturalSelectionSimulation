5b5487e3ab4ea079d796dbbf3e56ad5e901da39f
Jonathan Gamble <jig1314@gmail.com>
2022-02-27 11:31:30 -0500

    Added Screen Mode Display Option

--- Assets/NaturalSelectionSimulation/_Dev/DevJonathan/MenuController.cs
+++ Assets/NaturalSelectionSimulation/_Dev/DevJonathan/MenuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -12,24 +13,56 @@ namespace NaturalSelectionSimulation
         #region Display Settings
 
         [Header("Screen Mode Dropdown")]
-        private TMP_Dropdown screenModeDropdown;
+        public TMP_Dropdown screenModeDropdown;
+        private int _screenMode;
+        Dictionary<int, string> screenModes = new Dictionary<int, string>()
+        {
+            { 0, "Fullscreen" },
+            { 1, "Windowed" },
+            { 2, "Borderless" }
+        };
 
         #endregion
 
         private void Start()
         {
-            //screenModeDropdown.ClearOptions();
+            InitializeScreenModeDropdown();
+        }
 
-            //var screenModes = new List<string>()
-            //{
-            //    "Fullscreen",
-            //    "Windowed",
-            //    "Borderless"
-            //};
+        private void InitializeScreenModeDropdown()
+        {
+            screenModeDropdown.ClearOptions();
 
-            //screenModeDropdown.AddOptions(screenModes);
-            //screenModeDropdown.value = 0;
-            //screenModeDropdown.RefreshShownValue();
+            screenModeDropdown.AddOptions(screenModes.OrderBy(s => s.Key).Select(s => s.Value).ToList());
+
+            if (screenModes.ContainsKey(PlayerPrefs.GetInt("masterFullscreen", -1)))
+            {
+                _screenMode = PlayerPrefs.GetInt("masterFullscreen");
+                screenModeDropdown.value = _screenMode;
+            }
+            else
+            {
+                _screenMode = 0;
+                screenModeDropdown.value = 0;
+            }
+
+            screenModeDropdown.RefreshShownValue();
+        }
+
+        public void SetScreenMode(int screenMode)
+        {
+            _screenMode = screenMode;
+            PlayerPrefs.SetInt("masterFullscreen", screenMode);
+            Screen.fullScreen = screenModes[screenMode] == "Fullscreen";
+
+            if(screenModes[screenMode] == "Windowed")
+            {
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+            }
+            else if (screenModes[screenMode] == "Borderless")
+            {
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            }
         }
 
         public void ExitGame()
