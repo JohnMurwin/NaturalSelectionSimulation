964f5981127ef4014509774a6369c1b7a7e4c151
John Murwin <john.murwin@gmail.com>
2022-05-06 10:35:47 -0400

    basic orbit camera

---
+++ Assets/NaturalSelectionSimulation/Scripts/Cameras/Camera_Orbit.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace NaturalSelectionSimulation
+{
+    public class Camera_Orbit : MonoBehaviour
+    {
+        #region Private Variables
+        
+        [SerializeField][Tooltip("How fast the camera will rotate around target")]
+        private float _speed = 10f;
+        [SerializeField][Tooltip("What object the camera will rotate around. (Center 'LookTarget' in the map.")]
+        private Transform _target;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void FixedUpdate()  //? using FixedUpdate instead of Update/LateUpdate to ensure physics step is even for smooth camera rotate
+        {
+            transform.LookAt(_target);
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        }
+        #endregion
+    }
+}
